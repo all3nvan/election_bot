1eb82ef98bb92c802f78a6c52e606d6a76d920b5
Allen Van <all3nvan@gmail.com>
2016-03-24 21:59:55 -0700

    start and end election commands

--- lib/election_bot/bot.rb
+++ lib/election_bot/bot.rb
@@ -3,19 +3,43 @@ require 'discordrb'
 class ElectionBot::Bot
   def initialize(command_bot)
     @bot = command_bot
-    @election = Election.new
-    vote_command
+    start_command
     raffle_command
+    @bot.set_user_permission(ENV['BOT_OWNER_ID'].to_i, 2)
   end
 
   def run
     @bot.run
   end
 
+  def start_command
+    @bot.command(:start, election_command_attributes) do |event|
+      if election_channel?(event.channel.id)
+        @bot.remove_command(:start)
+        @election = Election.new
+        @winner_ids = nil
+        vote_command
+        end_command
+        vote_command_attributes[:description]
+      end
+    end
+  end
+
+  def end_command
+    @bot.command(:end, election_command_attributes) do |event|
+      if election_channel?(event.channel.id)
+        @bot.remove_command(:end)
+        @bot.remove_command(:vote)
+        start_command
+        @winner_ids = @election.winners
+      end
+    end
+  end
+
   def vote_command
     @bot.command(:vote, vote_command_attributes) do |event, username|
       # CHANNEL_ID from config file is interpreted as a string
-      if event.channel.id == ENV['CHANNEL_ID'].to_i
+      if election_channel?(event.channel.id)
         if @election.has_voted?(event.user.id)
           "You have already voted, #{event.user.username}!"
         elsif !user_exists?(username)
@@ -26,14 +50,6 @@ class ElectionBot::Bot
         end
       end
     end
-  end
-
-  def vote_command_attributes
-    {
-      description: 'Vote for the mayor (!vote username)',
-      min_args: 1,
-      max_args: 1
-    }
   end
 
   def user_exists?(username)
@@ -50,6 +66,25 @@ class ElectionBot::Bot
       .users
       .find { |_, user| user.username == username }
       .first
+  end
+
+  def vote_command_attributes
+    {
+      description: 'Vote for the mayor (!vote username)',
+      min_args: 1,
+      max_args: 1
+    }
+  end
+
+  def election_command_attributes
+    {
+      help_available: false,
+      permission_level: 2
+    }
+  end
+
+  def election_channel?(channel_id)
+    channel_id == ENV['CHANNEL_ID'].to_i
   end
 
   def raffle_command
